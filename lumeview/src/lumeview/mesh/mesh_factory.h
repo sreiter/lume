bb91511c89a8d76158087597f3e09447ae2251b1
Sebastian Reiter <s.b.reiter@gmail.com>
2019-07-30 20:41:48 +0200

    Added mesh factory

---
+++ lumeview/src/lumeview/mesh/mesh_factory.h
@@ -0,0 +1,43 @@
+// This file is part of lumeview, a lightweight viewer for unstructured meshes
+//
+// Copyright (C) 2019 Sebastian Reiter <s.b.reiter@gmail.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <lume/mesh.h>
+#include <lumeview/util/rect.h>
+
+namespace lumeview::mesh
+{
+
+class MeshFactory
+{
+public:
+    using Mesh = lume::Mesh;
+    using Rect = util::Rect;
+
+    static std::shared_ptr <Mesh> create_rect   (Rect const& area);
+    static std::shared_ptr <Mesh> create_rect_uv (Rect const& area, Rect const& uv);
+};
+
+}// end of namespace lumeview::mesh
